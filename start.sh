d5d9e18d6c7de8b32c0720da062c5915de871ec8
LmaoLover <hello@lmao.love>
2021-10-06 22:57:20 -0600

    Add infinite loop to start.sh to reboot after crashes

--- start.sh
+++ start.sh
@@ -2,4 +2,8 @@
 
 #java -Xmx4G -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M -jar fabric-server-launch.jar nogui
 
-java -Xmx4G -jar fabric-server-launch.jar nogui
+while true
+do
+  rlwrap java -Xmx4G -jar fabric-server-launch.jar nogui && break
+  sleep 1
+done
